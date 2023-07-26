9530e887830e747bbf9126f00e76c9737f38b390
Mwadali <wellingtonmwadali@gmail.com>
2023-07-26 19:21:17 +0300

    setup generated image

--- server/routes/dalleRoutes.js
+++ server/routes/dalleRoutes.js
@@ -6,29 +6,30 @@ dotenv.config();
 
 const router = express.Router();
 const configuration =  new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
+    apiKey:process.env.OPENAI_API_KEY,
 })
 const openai = new OpenAIApi(configuration);
 
-router.route('/').get((req, res) =>{
+router.route('/').get((req,res) =>{
     res.send('Testing OpenAi key')
 });
 
-router.route("/").post(async(req, res) => {
+router.route('/').post(async(req, res) => {
     try {
-        const {prompt} = req.body;
-
-        const aiResponse = await openai.createImage({
-            prompt,
-            n: 1,
-            size: '1024x1024',
-            response_format: 'b64_json',
-        });
-        const image = aiResponse.data.data[0].b64_json;
-        res.status(200).json({photo: image})
+      const {prompt} = req.body;
+  
+      const aiResponse = await openai.createImage({
+        prompt,
+        n: 1,
+        size: '1024x1024',
+        response_format: 'b64_json',
+      });
+  
+      const image = aiResponse.data.data[0].b64_json;
+      res.status(200).json({ photo: image });
     } catch (error) {
-        console.log("error")
-        res.status(500).send(error?.response.data.error.message)
+      console.error(error);
+      res.status(500).send(error?.response.data.error.message || 'Something went wrong');
     }
-})
+  });
 export default router;
